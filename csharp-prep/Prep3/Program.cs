a503e00d98181b128e72522116deb2e848d79619
eivarela <evans.varela.mejia@gmail.com>
2023-04-21 20:16:28 -0600

    prep1 & prep2 realizados

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -5,5 +5,13 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep3 World!");
+
+        Console.Write("What is the magic number?");
+        string userInput = Console.ReadLine();
+        int magicNumber = int.Parse(userInput);
+        
+
+
+
     }
 }
