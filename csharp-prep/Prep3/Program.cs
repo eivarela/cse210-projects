445ca180bd3f93bb970c33fb80b213c86fde3577
eivarela <evans.varela.mejia@gmail.com>
2023-04-21 21:53:57 -0600

    fase final de prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -14,6 +14,7 @@ class Program
         //int magicNumber = int.Parse(userMagicNumber);
 
         int guess = -1;
+        int guess_count = 1;
         
         while (guess != number)
         {
@@ -24,15 +25,56 @@ class Program
             if (guess < number)
             {
                 Console.WriteLine("Higher");
+                guess_count += 1;
+
             }
             else if (guess > number)
             {
                 Console.WriteLine("Lower");
+                guess_count += 1;
             }
         } 
         guess = number;
-        Console.Write("You guessed it!");
-        
+        Console.WriteLine("You guessed it!");
+        Console.Write($"You made {guess_count} guesse(s).");
 
+        Console.Write("Do you want to play again? (YES/NO) ");
+        string try_again = Console.ReadLine();
+        try_again = try_again.ToLower();
+
+        while (try_again == "yes")
+        {
+            number = randomGenerator.Next(1, 100);
+            guess = -1;
+            guess_count = 1;
+
+            while (guess != number)
+            {
+                Console.Write("What is your guess? ");
+                string userGuess = Console.ReadLine();
+                guess = int.Parse(userGuess);
+
+                if (guess < number)
+                {
+                    Console.WriteLine("Higher");
+                    guess_count += 1;
+
+                }
+                else if (guess > number)
+                {
+                    Console.WriteLine("Lower");
+                    guess_count += 1;
+                }
+            } 
+            guess = number;
+            Console.WriteLine("You guessed it!");
+            Console.WriteLine($"You made {guess_count} guesse(s).");
+            Console.WriteLine("Do you want to play again? (YES/NO) ");
+            try_again = Console.ReadLine();
+            try_again = try_again.ToLower();
+
+        }
+        try_again = "no";
+        Console.WriteLine("Thanks for playing");
     }
 }
