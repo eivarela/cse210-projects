53539d304e5fb8dc938dc47f74d3caa124cbdff6
eivarela <evans.varela.mejia@gmail.com>
2023-04-21 21:14:43 -0600

    magicNumber changes

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -6,9 +6,32 @@ class Program
     {
         Console.WriteLine("Hello Prep3 World!");
 
-        Console.Write("What is the magic number?");
-        string userInput = Console.ReadLine();
-        int magicNumber = int.Parse(userInput);
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 100);
+
+        //Console.Write("What is the magic number? ");
+        //string userMagicNumber = Console.ReadLine();
+        //int magicNumber = int.Parse(userMagicNumber);
+
+        int guess = -1;
+        
+        while (guess != number)
+        {
+            Console.Write("What is your guess? ");
+            string userGuess = Console.ReadLine();
+            guess = int.Parse(userGuess);
+
+            if (guess < number)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guess > number)
+            {
+                Console.WriteLine("Lower");
+            }
+        } 
+        guess = number;
+        Console.Write("You guessed it!");
 
     }
 }
